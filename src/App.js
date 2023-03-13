a8d59d2968f795d61f04d87bc1b727c832162e9f
JR Bautista <jrpbautista@gmail.com>
2023-03-13 10:41:55 +0800

    sample input in react

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ function App() {
   return (
     <div className="App">
       <label htmlFor="name">Name: </label>
-      <input type="text" name="name"></input>
+      <input type="text" id="name" name="name"></input>
     </div>
+
+    /* 
+      why htmlFor? 
+      In JSX, htmlFor reflects the 'for' content property.
+      
+      Source: https://developer.mozilla.org/en-US/docs/Web/API/HTMLLabelElement/htmlFor
+    */
   );
 }
 
