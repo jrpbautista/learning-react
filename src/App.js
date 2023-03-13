0fd5b7793577e357a22b3c1983ade1a7a841ee75
JR Bautista <jrpbautista@gmail.com>
2023-03-13 11:26:13 +0800

    adding states

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import "./App.css";
 import Input from "./components/Input";
 
 function App() {
-  return (
-    <div className="App">
-      <Input />
-    </div>
-  );
-
-  // here, we created a new component: Input in components folder
-  // which contains the input we created last branch
+    return (
+        <div className="App">
+            <Input />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,30 @@
+import { useState } from "react";
+
 const Input = () => {
-  return (
-    <div>
-      <label htmlFor="name">Name: </label>
-      <input type="text" id="name" name="name" />
-    </div>
-  );
+    const [name, setName] = useState();
+
+    // useState() lets you add a state in the component
+    // if destructured,
+    // 1st element is the state,
+    // 2nd element will be the dispatch action
+
+    const changeNameHandler = (event) => {
+        setName(event.target.value);
+    };
+
+    return (
+        <div>
+            <label htmlFor="name">Name: </label>
+            <input
+                type="text"
+                id="name"
+                name="name"
+                onChange={changeNameHandler}
+            />
+
+            <h1>{name}</h1>
+        </div>
+    );
 };
 
 export default Input;
