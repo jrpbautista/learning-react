54be770ac9045a0c33b8a34553c84373ef2bbe31
JR Bautista <jrpbautista@gmail.com>
2023-03-13 11:44:12 +0800

    sample conditional rendering

--- src/components/Input.js
+++ src/components/Input.js
@@ -7,12 +7,21 @@ const Input = () => {
         setCounter((prevState) => prevState + 1);
     };
 
+    const decreaseCounterHandler = () => {
+        setCounter((prevState) => prevState - 1);
+    };
+
     return (
         <div>
+            <p>Count to 5</p>
             <h1>{counter}</h1>
-            <button onClick={increaseCounterHandler}>Increase count</button>
+            <button onClick={increaseCounterHandler}>Increase</button>
+            <button onClick={decreaseCounterHandler}>Decrease</button>
+            {counter >= 5 && <h1>DONE!</h1>}
         </div>
     );
+
+    // added h1 that will only be shown if counter is equal or greater than 5.
 };
 
 export default Input;
